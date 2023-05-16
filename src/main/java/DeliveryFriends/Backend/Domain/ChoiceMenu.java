3e1c53fb9053bfe1b12c10464586f652d16e5c7e
JaeHwanCho <makefuture@kakao.com>
2023-05-16 22:09:16 +0900

    SETUP : security config & make tables

---
+++ src/main/java/DeliveryFriends/Backend/Domain/ChoiceMenu.java
@@ -0,0 +1,28 @@
+package DeliveryFriends.Backend.Domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+public class ChoiceMenu extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    Long count;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    User user;
+
+    @ManyToOne
+    @JoinColumn(name = "menu_id")
+    Menu menu;
+
+    @ManyToOne
+    @JoinColumn(name = "choice_option_id")
+    ChoiceOption choiceOption;
+}
---
+++ src/main/java/DeliveryFriends/Backend/Domain/Review.java
@@ -0,0 +1,25 @@
+package DeliveryFriends.Backend.Domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+public class Review extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    Long score;
+    String content;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    User user;
+}
